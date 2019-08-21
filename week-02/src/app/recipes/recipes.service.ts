b6a2e745adacb433c019a63a1c4929604e6a55b4
LABC710-21 <LABC710-21@EXAM.UMN.AC.ID>
2019-08-21 16:39:59 +0700

    Week02

---
+++ week-02/src/app/recipes/recipes.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipes.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = [
+    {
+      id: '1',
+      title: "Gado Gado",
+      imageUrl: "https://cdn.idntimes.com/content-images/community/2018/04/big-slider-gado-gado-enak-dan-lezat-d15989179c334422677c80293a7b51c0_600x400.jpg",
+      ingredients: ['Ketupat', 'Bumbu-Kacang', 'Timun', 'Tauge', 'Bihun']
+    },
+    {
+      id: '2',
+      title: "Mie Ayam",
+      imageUrl: "http://www.dapurkobe.co.id/wp-content/uploads/mie-goreng-saus-tiram.jpg",
+      ingredients: ['Mie']
+    }
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: String) {
+    return {
+      ...this.recipes.find(recipe => recipe.id === recipeId)
+    };
+  }
+
+  deleteRecipe(recipeId: String) {
+    for(var i=0; i < this.recipes.length; i++) {
+      if(this.recipes[i].id == recipeId) {
+        this.recipes.splice(i, 1);
+      }
+    }
+  }
+}
